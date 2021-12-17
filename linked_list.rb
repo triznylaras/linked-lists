fed8f495f604bc01803cc85b7232ff46a3e34fdb
triznylaras <triznylaras@gmail.com>
2021-12-17 16:11:24 +0700

    create linked list and node classes, add append, prepend, size methods

---
+++ linked_list.rb
@@ -0,0 +1,63 @@
+require_relative 'node'
+require 'pry-byebug'
+
+class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    @head ||= new_node
+    if !@tail
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+    end
+    @tail = new_node
+  end
+
+  def prepend(value)
+    new_node = Node.new(value, @head)
+    @head = new_node
+  end
+
+  def size
+    counter = 0
+    current_node = @head
+    until current_node.nil?
+      counter += 1
+      current_node = current_node.next_node
+    end
+    counter
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+end
+my_list = LinkedList.new
+puts my_list
+my_list.append('last')
+puts my_list
+my_list.prepend('first')
+puts my_list
+puts "Size: #{my_list.size}"
---
+++ node.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :data, :next_node
+  
+  def initialize(data = nil, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
+end
